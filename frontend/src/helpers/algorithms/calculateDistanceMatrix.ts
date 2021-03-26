9df8b161649064907e04d9e68e504c568d89b30c
Hod04 <omerhod@gmail.com>
2021-03-26 13:59:05 +0100

    when calculating the distance matrix, return early if distance === 0

--- frontend/src/helpers/algorithms/calculateDistanceMatrix.ts
+++ frontend/src/helpers/algorithms/calculateDistanceMatrix.ts
@@ -2,9 +2,9 @@ import _ from "lodash";
 
 export function calculateDistanceMatrix(
   data: number[][],
-  distanceFn: (p: number[], q: number[], Wa: number, We: number) => number,
+  distanceFn: (p: number[], q: number[], Wa: number[], We: number) => number,
   // attribute weight
-  Wa: number,
+  Wa: number[],
   // edge class weight
   We: number
 ) {
@@ -17,6 +17,11 @@ export function calculateDistanceMatrix(
 
   for (let i = 0; i < data.length; i++) {
     for (let j = 0; j <= i; j++) {
+      if (i === j) {
+        distanceMatrix[i][j] = 0;
+        distanceMatrix[j][i] = 0;
+        continue;
+      }
       distanceMatrix[i][j] = distanceFn(data[i], data[j], Wa, We);
       distanceMatrix[j][i] = distanceMatrix[i][j];
     }
@@ -37,12 +42,12 @@ export function calculateDistanceMatrix(
 export function euclideanDistance(
   p: number[],
   q: number[],
-  Wa: number,
+  Wa: number[],
   We: number
-) {
+): number {
   let distance: number = 0;
   for (let i = 0; i < p.length; i++) {
-    distance += Wa * We * (p[i] - q[i]) * (p[i] - q[i]);
+    distance += Wa[i] * We * Math.pow(p[i] - q[i], 2);
   }
 
   return Math.sqrt(distance);
@@ -55,6 +60,10 @@ export function manhattanDistance(
   return Math.abs(pointA.x - pointB.x) + Math.abs(pointA.y - pointB.y);
 }
 
+export function mDistance(p: number, q: number): number {
+  return Math.abs(p - q);
+}
+
 export function getDistanceRange(
   distanceMatrix: number[][]
 ): { min: number; max: number } {
@@ -63,9 +72,10 @@ export function getDistanceRange(
     max: number;
   };
   _.each(distanceMatrix, (row) => {
-    const minDist: number | undefined = _.min(row);
-    const maxDist: number | undefined = _.max(row);
-
+    let rowClone: number[] = _.clone(row);
+    rowClone.splice(_.indexOf(rowClone, 0), 1);
+    const minDist: number | undefined = _.min(rowClone);
+    const maxDist: number | undefined = _.max(rowClone);
     if (
       distanceRange.min == null ||
       (minDist != null && minDist < distanceRange.min)
@@ -80,6 +90,7 @@ export function getDistanceRange(
       distanceRange.max = maxDist as number;
     }
   });
+
   return distanceRange;
 }
 
@@ -93,9 +104,14 @@ function normalizeValues(
       if (value === 0) {
         return;
       }
+      if (value === distanceRange.min) {
+        distanceMatrixClone[rowIndex][valueIndex] = value;
+        return;
+      }
       distanceMatrixClone[rowIndex][valueIndex] =
         (value - distanceRange.min) / (distanceRange.max - distanceRange.min);
     });
   });
+
   return distanceMatrixClone;
 }
