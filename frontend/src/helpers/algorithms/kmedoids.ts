66b6caaed1cdca2435501cd79e68c286eb86fd81
Hod04 <omerhod@gmail.com>
2021-03-26 13:59:49 +0100

    add medoid object

--- frontend/src/helpers/algorithms/kmedoids.ts
+++ frontend/src/helpers/algorithms/kmedoids.ts
@@ -1,5 +1,5 @@
 import _ from "lodash";
-import { Clusterer } from "k-medoids";
+import { Cluster, Clusterer } from "k-medoids";
 import { SharedTypes } from "../../shared/sharedTypes";
 
 export function kmedoids(
@@ -7,33 +7,56 @@ export function kmedoids(
   nodes: SharedTypes.Graph.INode[],
   k: number
 ): {
-  [nodeId: number]: {
-    medoid: number;
-    distanceFromMedoid: number;
+  nodeMedoidInfoObjects: {
+    [nodeId: number]: {
+      medoid: number;
+      distanceFromMedoid: number;
+    };
   };
+  medoidObject: { [medoid: number]: number };
 } {
-  const clusterer = Clusterer.getInstance(distanceMatrix, k);
+  const clusterer: Clusterer<number[]> = Clusterer.getInstance(
+    distanceMatrix,
+    k
+  );
   clusterer.getClusteredData();
+
   let nodeMedoidInfoObjects: {
     [nodeId: number]: {
       medoid: number;
       distanceFromMedoid: number;
     };
   } = {};
-  _.each(clusterer.Clusters, (cluster, index) => {
+
+  let medoidObject: {
+    [medoid: number]: number;
+  } = {};
+
+  const clusters: Cluster<number[]>[] = clusterer.Clusters.sort((a, b) => {
+    let sortable: number = a.Elements.length - b.Elements.length;
+    _.each(a.Elements, (elem) => {
+      sortable += _.indexOf(elem.Element, 0);
+    });
+    _.each(b.Elements, (elem) => {
+      sortable -= _.indexOf(elem.Element, 0);
+    });
+    return sortable;
+  });
+
+  _.each(clusters, (cluster, index) => {
     _.each(cluster.Elements, (element) => {
       const medoid: number = index;
-      const nodeIndex: number = getElementIndex(element.Element);
+      const nodeIndex: number = _.indexOf(element.Element, 0);
       const node: SharedTypes.Graph.INode = nodes[nodeIndex];
       nodeMedoidInfoObjects[node.id] = {
         medoid,
         distanceFromMedoid: element.DistanceFromMedoid,
       };
+      if (element.DistanceFromMedoid === 0) {
+        medoidObject[medoid] = nodeIndex;
+      }
     });
   });
-  return nodeMedoidInfoObjects;
-}
 
-function getElementIndex(row: number[]): number {
-  return _.indexOf(row, 0);
+  return { nodeMedoidInfoObjects, medoidObject };
 }
